b9c856f44afdc24e9a1b91140caa3a7c3bf63bb1
Kaveendra Randeera <khkrandeera@gmail.com>
2023-11-22 16:17:52 +0530

    Complete schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,4 +9,10 @@ CREATE TABLE IF NOT EXISTS course(
     name VARCHAR(50) NOT NULL,
     duration_in_months INT NOT NULL
 );
-CREATE TABLE;
+CREATE TABLE IF NOT EXISTS teacher_course(
+    teacher_id INT NOT NULL ,
+    course_id INT NOT NULL ,
+    CONSTRAINT PRIMARY KEY (teacher_id, course_id),
+    CONSTRAINT fk_1 FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+    CONSTRAINT fk_1 FOREIGN KEY (course_id) REFERENCES course(id)
+);
